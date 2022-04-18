35db27d1c5a5a60bd4ce663fc33401e11beea42c
Romil Jain <61315825+jainromil56@users.noreply.github.com>
2022-04-18 14:17:21 +0530

    Used yargs for argv(arguments) from terminals
    yarn add yargs - to add yargs an npm library
    used builder, handler provided by yargs
    node app.js add --title="shopping list" --body="my new note" - use this command in terminal to add note

--- app.js
+++ app.js
@@ -1,18 +1,70 @@
 const chalk = require('chalk')
 const getNotes = require('./notes.js')
+const yargs = require('yargs')
 
 // process.argv will give us an array, and then we choose 3rd element from that array which is 
 // our argument
 // if you pass more commands then it will be in this array too
 const command = process.argv[2]
 
-console.log(process.argv)
+// console.log(process.argv)
 
-if (command === 'add') {
-    const addingNote = chalk.green.bold('Adding Note')
-    // const addingNote = chalk.green.inverse.bold('Adding Note')
-    console.log(addingNote)
-} else if (command === 'remove') {
-    const removingNote = chalk.red.bold('Removing Note')
-    console.log(removingNote)
-}
+
+//created add command with yargs
+yargs.command({
+    'command':'add',
+    'describe':'adds a note',
+    // builder is used to add additional commands - node app.js add --title="shopping list" --body="my new note"
+    builder: {
+        'title':{
+            'describe':'Note title',
+            // below option will set it as required, so only add will not work
+            // need to write full - node app.js add --title="Shopping list"
+            'demandOption':true,
+            'type':'string'
+        },
+        'body':{
+            'describe':'Note content',
+            'demandOption':true,
+            'type':'string'
+        }
+    },
+    // passing argument i.e. argv will give us input of command from handler
+    handler: function(argv){
+        const addingNote = chalk.green.bold('Adding Note')
+        const displayNote = chalk.cyan("title : "+argv.title, '\n','body : '+argv.body)
+        console.log(addingNote)
+        console.log(displayNote)
+    }
+})
+
+//created remove command
+yargs.command({
+    'command':'remove',
+    'describe':'To remove note',
+    handler: function(){
+        console.log(chalk.red('Removing note'))
+    }
+})
+
+//created list command
+yargs.command({
+    'command':'list',
+    'describe':'To list notes',
+    handler: function(){
+        console.log(chalk.cyan('Listing note'))
+    }
+})
+
+//created read command
+yargs.command({
+    'command':'read',
+    'describe':'To read notes',
+    handler: function(){
+        console.log(chalk.magenta('Reading note'))
+    }
+})
+
+// console.log(yargs.argv)
+yargs.parse()
+
