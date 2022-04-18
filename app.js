d375eb8c0339f8fffa99a941a70b96aea2cfa72c
Romil Jain <61315825+jainromil56@users.noreply.github.com>
2022-04-18 12:02:21 +0530

    made some minor changes

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const chalk = require('chalk')
 const getNotes = require('./notes.js')
 
+// process.argv will give us an array, and then we choose 3rd element from that array which is 
+// our argument
+// if you pass more commands then it will be in this array too
 const command = process.argv[2]
 
 console.log(process.argv)
@@ -10,5 +13,6 @@ if (command === 'add') {
     // const addingNote = chalk.green.inverse.bold('Adding Note')
     console.log(addingNote)
 } else if (command === 'remove') {
-    console.log('Removing note!')
+    const removingNote = chalk.red.bold('Removing Note')
+    console.log(removingNote)
 }
